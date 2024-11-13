86fbfaa20665530bab5770ad90f4497f24f22ce1
hanakonako42 <iori.42.0001@gmail.com>
2024-11-13 21:19:21 +0900

    inheritance

--- typescript-workshop/testBasicClass5.ts
+++ typescript-workshop/testBasicClass5.ts
@@ -10,4 +10,21 @@ class Person {
     }
 }
 
-class Teacher
+class Teacher extends Person {
+    subject: string;
+    constructor(subject: string, firstName = "AWS", lastName = "太郎") {
+        super(firstName, lastName);
+        this.subject = subject;
+    }
+    greet() {
+        console.log(`私は${this.firstName} ${this.lastName}です。${this.subject}の先生をやっています。`);
+    }
+}
+
+const taro = new Teacher("国語");
+const amazon = new Teacher("数学", "Amazon");
+const hanako = new Teacher("美術", "山田", "華子");
+
+taro.greet();
+jiro.greet();
+hanako.greet();
